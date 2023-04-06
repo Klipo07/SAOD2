27b04177fe693f62f15b2102e2445c9451efc660
Kirill Ogarkov <Klipo07@example.com>
2023-04-06 19:52:59 +0700

    First experiment

--- lib/bstree.h
+++ lib/bstree.h
@@ -1,14 +1,11 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
 
 struct bstree {
-    char *key;
-    uint32_t value;
-    struct bstree *left;
-    struct bstree *right;
+  char *key;
+  uint32_t value;
+  struct bstree *left;
+  struct bstree *right;
 };
 
 struct bstree *bstree_create(char *key, int value);
@@ -17,3 +14,4 @@ struct bstree *bstree_lookup(struct bstree *tree, char *key);
 struct bstree *bstree_delete(struct bstree *tree, char *key);
 struct bstree *bstree_min(struct bstree *tree);
 struct bstree *bstree_max(struct bstree *tree);
+void bstree_free(struct bstree* tree);
--- src/bstree.c
+++ src/bstree.c
@@ -1,80 +1,164 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "../lib/bstree.h"
-#include "../lib/hashtab.h"
 
-struct bstree *bstree_create(char *key, int value)
-{
-    struct bstree *v = (struct bstree*)malloc(sizeof(struct bstree));
-    if(!v){
-        return NULL;
-    }
+struct bstree *bstree_create(char *key, int value) {
+  struct bstree *v = (struct bstree *)malloc(sizeof(struct bstree));
+  if (!v) {
+    return NULL;
+  }
 
-    v->key = key;
-    v->value = value;
-    v->left = NULL;
-    v->right = NULL;
+  v->key = key;
+  v->value = value;
+  v->left = NULL;
+  v->right = NULL;
 
-    return v;
+  return v;
 }
-void bstree_add(struct bstree *tree, char *key, int value)
-{
-    if(!tree){
-        return;
-    }
-    struct bstree *v = bstree_create(key, value);
-
-    if (!v){
-        return;
-    }
-    struct bstree *parent;
-    while(tree != NULL){
-        parent = tree;
-        if (value > tree->value){
-            tree = tree->right;
-        }
-        else{
-            tree = tree->left;
-        }
-    }
-    if(parent->value < value){
-        parent->right = v;
-    }
-    else{
-       parent->left = v; 
-    }
-
+void bstree_add(struct bstree *tree, char *key, int value) {
+  if (tree == NULL)
+    return;
+  struct bstree *parent, *node;
+  while (tree != NULL) {
+    parent = tree;
+    if (strcmp(key, tree->key) < 0)
+      tree = tree->left;
+    else if (strcmp(key, tree->key) > 0)
+      tree = tree->right;
+    else
+      return;
+  }
+  node = bstree_create(key, value);
+  if (strcmp(key, parent->key) < 0)
+    parent->left = node;
+  else
+    parent->right = node;
 }
-struct bstree *bstree_lookup(struct bstree *tree, char *key)
-{
-    while (tree != NULL) {
-        if (key == tree->key)
-            return tree;
-        else if (key < tree->key)
-            tree = tree->left;
-        else
-            tree = tree->right;
-    }
 
+struct bstree *bstree_lookup(struct bstree *tree, char *key) {
+  while (tree != NULL) {
+    if (strcmp(key, tree->key) == 0)
+      return tree;
+    else if (strcmp(key, tree->key) < 0)
+      tree = tree->left;
+    else
+      tree = tree->right;
+  }
+  return tree;
+}
+
+struct bstree *bstree_min(struct bstree *tree) {
+  if (!tree)
+    return NULL;
+
+  while (tree->left != NULL)
+    tree = tree->left;
+  return tree;
+}
+struct bstree *bstree_max(struct bstree *tree) {
+  if (!tree)
+    return NULL;
+
+  while (tree->right != NULL)
+    tree = tree->right;
+  return tree;
+}
+
+struct bstree *bstree_delete(struct bstree *tree, char *key) {
+  if (!tree)
+    return NULL;
+
+  struct bstree *parent = NULL;
+  struct bstree *node = tree;
+
+  // Find the node to be deleted and its parent
+  while (node) {
+    if (strcmp(key, node->key) == 0) {
+      break;
+    } else if (strcmp(key, node->key) < 0) {
+      parent = node;
+      node = node->left;
+    } else {
+      parent = node;
+      node = node->right;
+    }
+  }
+
+  // If the node was not found, return the original tree
+  if (!node) {
     return tree;
-}
-struct bstree *bstree_delete(struct bstree *tree, char *key)
-{
+  }
 
-}
-struct bstree *bstree_min(struct bstree *tree)
-{
-    if (tree == NULL)
-        return NULL;
+  // If the node has no children, just remove it
+  if (!node->left && !node->right) {
+    if (!parent) {
+      // The node is the root of the tree
+      free(node);
+      return NULL;
+    } else if (parent->left == node) {
+      parent->left = NULL;
+    } else {
+      parent->right = NULL;
+    }
+    free(node);
+  }
+  // If the node has one child, replace it with its child
+  else if (!node->left || !node->right) {
+    struct bstree *child = node->left ? node->left : node->right;
+    if (!parent) {
+      // The node is the root of the tree
+      free(node);
+      return child;
+    } else if (parent->left == node) {
+      parent->left = child;
+    } else {
+      parent->right = child;
+    }
+    free(node);
+  }
+  // If the node has two children, replace it with the minimum node in its
+  // right subtree
+  else {
+    struct bstree *successor = bstree_min(node->right);
+    node->key = successor->key;
+    node->value = successor->value;
+    tree->right = bstree_delete(tree->right, successor->key);
+  }
 
-    while (tree->left != NULL)
-        tree = tree->left;
-    return tree;
+  return tree;
 }
-struct bstree *bstree_max(struct bstree *tree)
-{
-    if (tree == NULL)
-        return NULL;
 
-    while (tree->right != NULL)
-        tree = tree->right;
-    return tree;
+void bstree_free(struct bstree *tree) {
+  size_t capacity = 1024;
+  size_t top = 0;
+  struct bstree **stack =
+      (struct bstree **)malloc(capacity * sizeof(struct bstree *));
+
+  if (!tree)
+    return;
+
+  stack[top++] = tree;
+
+  while (top > 0) {
+    struct bstree *current = stack[--top];
+
+    if (current->left)
+      stack[top++] = current->left;
+    if (current->right)
+      stack[top++] = current->right;
+
+    // Увеличиваем размер стека, если он заполнен
+    if (top == capacity) {
+      capacity *= 2;
+      stack =
+          (struct bstree **)realloc(stack, capacity * sizeof(struct bstree *));
+    }
+
+    free(current);
+  }
+
+  free(stack);
 }
