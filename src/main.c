18945e3af377f2ee2f18232adb719b6767afe47f
Kirill Ogarkov <Klipo07@example.com>
2023-04-06 21:44:25 +0700

    Second Experiment

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ struct listnode *hashtab[Hashtab_Size];
 char words[MaxKeys][MaxKeyLenght];
 
 int main() {
-  struct bstree *tree, *node;
-  struct listnode *hash, *node_hash;
+  struct bstree *tree;
+  struct listnode *hash;
 
   double time_bstree = 0.0, time_hash = 0.0;
   clock_t begin, end;
@@ -32,24 +32,27 @@ int main() {
   hashtab_add(hashtab, words[0], 0);
   tree = bstree_create(words[0], 0);
   for (int i = 1; i < MaxKeys; i++) {
-    bstree_add(tree, words[i], i);
-    hashtab_add(hashtab, words[i], i);
     if ((i + 1) % 10000 == 0) {
       char *rand_key = words[getrand(0, i)];
       // bstree
       begin = clock();
-      node = bstree_lookup(tree, rand_key);
+      bstree_add(tree, rand_key, i);
       end = clock();
       time_bstree = (double)(end - begin) / CLOCKS_PER_SEC;
 
       // hastab
       begin = clock();
-      node_hash = hashtab_lookup(hashtab, rand_key);
+      hashtab_add(hashtab, rand_key, i);
       end = clock();
       time_hash = (double)(end - begin) / CLOCKS_PER_SEC;
 
       printf("%f\t%f %20.25s %20.25s %20.25s\t\t%d\n", time_bstree, time_hash,
-             rand_key, node->key, node_hash->key, node->value);
+             rand_key);
+    }
+    else
+    {
+      bstree_add(tree, words[i], i);
+      hashtab_add(hashtab, words[i], i);
     }
   }
 
